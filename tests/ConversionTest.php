0bf402000667f867fe7daeeb6118e9482863a352
Roman Zipp <ich@ich.wtf>
2023-02-08 17:36:31 +0100

    Add json conversion test

--- tests/ConversionTest.php
+++ tests/ConversionTest.php
@@ -3,6 +3,8 @@
 namespace romanzipp\LaravelDTO\Tests;
 
 use romanzipp\LaravelDTO\AbstractModelData;
+use romanzipp\LaravelDTO\Attributes\Conversions\ConvertsFromEnum;
+use romanzipp\LaravelDTO\Attributes\Conversions\ConvertsToJson;
 use romanzipp\LaravelDTO\Attributes\ForModel;
 use romanzipp\LaravelDTO\Attributes\ModelAttribute;
 use romanzipp\LaravelDTO\Tests\Enums\IntegerEnum;
@@ -13,6 +15,19 @@ class ConversionTest extends TestCase
     public function testEnumConvertedToValue()
     {
         $data = new #[ForModel(SampleModel::class)] class(['foo' => IntegerEnum::FOO]) extends AbstractModelData {
+            #[ModelAttribute('foo'), ConvertsFromEnum]
+            public IntegerEnum $foo;
+        };
+
+        $model = $data->toModel();
+
+        self::assertSame($data->foo, IntegerEnum::FOO);
+        self::assertSame(1, $model->foo);
+    }
+
+    public function testEnumConvertedToValueWithoutAttribute()
+    {
+        $data = new #[ForModel(SampleModel::class)] class(['foo' => IntegerEnum::FOO]) extends AbstractModelData {
             #[ModelAttribute('foo')]
             public IntegerEnum $foo;
         };
@@ -21,5 +36,18 @@ class ConversionTest extends TestCase
 
         self::assertSame($data->foo, IntegerEnum::FOO);
         self::assertSame(1, $model->foo);
+    }
+
+    public function testConvertToJson()
+    {
+        $data = new #[ForModel(SampleModel::class)] class(['foo' => ['foo' => 'bar']]) extends AbstractModelData {
+            #[ModelAttribute('foo'), ConvertsToJson]
+            public array $foo;
+        };
+
+        $model = $data->toModel();
+
+        self::assertSame(['foo' => 'bar'], $data->foo);
+        self::assertSame('{"foo":"bar"}', $model->foo);
     }
 }
